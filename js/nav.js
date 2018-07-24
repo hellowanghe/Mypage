9159ade4576a2a5a5d6c43fea9af6da545127c1c
wanghe <1341511075@qq.com>
2018-07-24 13:42:18 +0800

    start

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,6 @@
 Vue.component('nav-counter', {
 	 props: ['todos'],
-  template: '<div><li><a href="/mypage/index.html">HOME</a></li><li><a href="/mypage/html/life/index.html">LIFE</a></li><li><a href="/mypage/html/note/index.html">NOTE</a></li><li><a href="/mypage/html/others/index.html">OTHER</a></li></div>'
+  template: '<div><li><a href="/index.html">HOME</a></li><li><a href="/html/life/index.html">LIFE</a></li><li><a href="/html/note/index.html">NOTE</a></li><li><a href="/html/others/index.html">OTHER</a></li></div>'
 });
 new Vue({ el: '#nav-counter' 
   });
