ba1b34fd80ba7a4622b032d3351348acd9e2c84b
wanghe <1341511075@qq.com>
2018-09-12 19:04:09 +0800

    new boot

--- js/nav.js
+++
@@ -1,6 +0,0 @@
-Vue.component('nav-counter', {
-	 props: ['todos'],
-  template: '<div><li><a href="/index.html">HOME</a></li><li><a href="/html/life/index.html">LIFE</a></li><li><a href="/html/note/index.html">NOTE</a></li><li><a href="/html/others/index.html">OTHER</a></li></div>'
-});
-new Vue({ el: '#nav-counter' 
-  });
